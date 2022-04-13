e06a41e7913e60eac4b95e6c22e388dfa2429994
Newt1712 <ankvhe150757@fpt.edu.vn>
2022-04-13 10:53:25 +0700

    CRUD implement

--- angular/src/app/book/book.component.ts
+++ angular/src/app/book/book.component.ts
@@ -1,24 +1,93 @@
 import { ListService, PagedResultDto } from '@abp/ng.core';
 import { Component, OnInit } from '@angular/core';
-import { BookDto, BookService } from '@proxy';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BookService, BookDto } from '@proxy';
+import { bookTypeOptions } from '@proxy/books';
+import { NgbDateNativeAdapter, NgbDateAdapter } from '@ng-bootstrap/ng-bootstrap';
+import { Confirmation, ConfirmationService } from '@abp/ng.theme.shared';
 
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.scss'],
-  providers: [ListService],
+  providers: [ListService, { provide: NgbDateAdapter, useClass: NgbDateNativeAdapter }],
 })
 export class BookComponent implements OnInit {
   book = { items: [], totalCount: 0 } as PagedResultDto<BookDto>;
 
-  constructor(public readonly list: ListService, private bookService: BookService) {}
+  selectedBook = {} as BookDto; // declare selectedBook
+  form: FormGroup; // add this line
+
+  // add bookTypes as a list of BookType enum members
+  bookTypes = bookTypeOptions;
+  isModalOpen = false; // add this line
+
+  constructor(
+    public readonly list: ListService,
+    private bookService: BookService,
+    private fb: FormBuilder, // inject FormBuilder
+    private confirmation: ConfirmationService // inject the ConfirmationService
+  ) {}
 
   ngOnInit() {
-    const bookStreamCreator = (query) => this.bookService.getList(query);
+    const bookStreamCreator = query => this.bookService.getList(query);
 
-    this.list.hookToQuery(bookStreamCreator).subscribe((response) => {
+    this.list.hookToQuery(bookStreamCreator).subscribe(response => {
       this.book = response;
+    });
+  }
+
+  // Add new method
+  createBook() {
+    this.selectedBook = {} as BookDto; // reset the selected book
+    this.buildForm(); // add this line
+    this.isModalOpen = true;
+  }
+
+  // Add editBook method
+  editBook(id: string) {
+    this.bookService.get(id).subscribe((book) => {
+      this.selectedBook = book;
+      this.buildForm();
+      this.isModalOpen = true;
+    });
+  }
+  // Add a delete method
+  deleteBook(id: string) {
+    this.confirmation.warn('::AreYouSureToDelete', '::AreYouSure').subscribe((status) => {
+      if (status === Confirmation.Status.confirm) {
+        this.bookService.delete(id).subscribe(() => this.list.get());
+      }
+    });
+  }
+
+
+  // add buildForm method
+  buildForm() {
+    this.form = this.fb.group({
+      name: [this.selectedBook.name || '', Validators.required],
+      type: [this.selectedBook.type || null, Validators.required],
+      publishDate: [
+        this.selectedBook.publishDate ? new Date(this.selectedBook.publishDate) : null,
+        Validators.required,
+      ],
+      price: [this.selectedBook.price || null, Validators.required],
+    });
+  }
+
+  save() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    const request = this.selectedBook.id
+      ? this.bookService.update(this.selectedBook.id, this.form.value)
+      : this.bookService.create(this.form.value);
+
+    request.subscribe(() => {
+      this.isModalOpen = false;
+      this.form.reset();
+      this.list.get();
     });
   }
 }
